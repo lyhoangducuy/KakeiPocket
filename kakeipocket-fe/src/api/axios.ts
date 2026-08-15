4c5a4521b14bb44796311deae493fec4905562ac
lyhoangducuy <lyhoangducuy@gmail.com>
2026-08-15 14:42:32 +0700

    proxy api through vercel

--- kakeipocket-fe/src/api/axios.ts
+++ kakeipocket-fe/src/api/axios.ts
@@ -11,7 +11,7 @@ export const setUnauthorizedHandler = (
 };
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
