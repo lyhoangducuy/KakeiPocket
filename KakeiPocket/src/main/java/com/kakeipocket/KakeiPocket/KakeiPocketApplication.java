331c8ccf4d6930e3427f5a18a897908942136eca
lyhoangducuy <lyhoangducuy@gmail.com>
2026-08-15 17:54:08 +0700

    fix ai final v.2.0

--- KakeiPocket/src/main/java/com/kakeipocket/KakeiPocket/KakeiPocketApplication.java
+++ KakeiPocket/src/main/java/com/kakeipocket/KakeiPocket/KakeiPocketApplication.java
@@ -11,10 +11,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class KakeiPocketApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        dotenv.entries().forEach(entry -> 
-            System.setProperty(entry.getKey(), entry.getValue())
-        );
 		SpringApplication.run(KakeiPocketApplication.class, args);
 	}
 
