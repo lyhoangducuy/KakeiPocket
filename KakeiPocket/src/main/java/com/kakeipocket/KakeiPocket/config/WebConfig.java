0f77e3bda04da4f23aee38fad714e408970de2da
lyhoangducuy <lyhoangducuy@gmail.com>
2026-08-15 14:16:30 +0700

    fix log

--- KakeiPocket/src/main/java/com/kakeipocket/KakeiPocket/config/WebConfig.java
+++ KakeiPocket/src/main/java/com/kakeipocket/KakeiPocket/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:5173",
-                        "https://kakei-pocket.vercel.app/"
+                        "https://kakei-pocket.vercel.app"
                 )
                 .allowedMethods(
                         "GET",
